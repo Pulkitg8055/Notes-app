747a0ee33798650e71795c579bbf3ecd7284e102
Pulkit Gupta <pulkitg8055@gmail.com>
2020-12-21 16:47:59 +0530

    Implement Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Header from './Header/Header';
 
 export default function App() {
-	return <div>Hello</div>;
+	return (
+		<div className='App'>
+			<Header dark={true}>GFG</Header>
+		</div>
+	);
 }
