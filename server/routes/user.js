5b8df7cadc1a7ee91e8df7df553f7eb7c54ed346
Pulkit Gupta <pulkitg8055@gmail.com>
2020-12-24 12:31:34 +0530

    Created user Route with sample

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,37 @@
-const app = require('express').Router();
-
-app.get('/', (req, res) => {
-	res.json('Welcome to /api/user!');
+const user = require('express').Router();
+const Users = {
+	Praveen: 'Hello@123',
+	Pulkit: 'Hello',
+	Shashi: '123456',
+};
+user.get('/', (req, res) => {
+	res.json(req.session.user);
 });
 
-module.exports = app;
+user.post('/login', (req, res) => {
+	const { username, password } = req.body;
+	if (!Users[username]) {
+		req.session.destroy();
+		res.status(403).json({
+			Error: true,
+			Success: false,
+			Message: 'User not found',
+		});
+	} else if (Users[username] && Users[username] !== password) {
+		res.status(403).json({
+			Error: true,
+			Success: false,
+			Message: 'Invalid password and username',
+		});
+	} else {
+		res.status(200).json({
+			Error: false,
+			Success: true,
+			Message: {
+				Name: username,
+			},
+		});
+	}
+});
+
+module.exports = user;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const api = require('./routes/api');
 const app = express();
 const port = 3100;
-
+const session = require('express-session');
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -11,6 +11,15 @@ app.listen(port, () => {
 	console.log(`Server started at port ${port}`);
 });
 app.use('/api', api);
+
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false },
+	})
+);
 
 app.get('/', (req, res) => {
 	res.json('Welcome');
