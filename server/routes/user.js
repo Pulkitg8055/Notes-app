81aa501139afc8db7a6d56f20cb05414f93e1593
Pulkit Gupta <pulkitg8055@gmail.com>
2020-12-24 13:36:21 +0530

    middleware position error corrected

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,30 +1,48 @@
 const user = require('express').Router();
-const Users = {
-	Praveen: 'Hello@123',
-	Pulkit: 'Hello',
-	Shashi: '123456',
-};
-user.get('/', (req, res) => {
-	res.json(req.session.user);
-});
 
+// Dummy list of users for now.
+const Users = {
+	Praveen: 'Hello123',
+	Bhooshan: 'dark456',
+	Rishav: 'ris2000',
+	Shivam: 'password',
+	Rajan: 'rks12345',
+	Isabel: 'coolcats123',
+	Shashi: 'akcd@123',
+};
+
+user.get('/', (req, res) => {
+	if (!!req.session.User) {
+		res.json(req.session.User);
+	} else {
+		res.status(401).json({
+			Error: true,
+			Success: false,
+			Message: 'Not Logged In!',
+		});
+	}
+});
 user.post('/login', (req, res) => {
 	const { username, password } = req.body;
 	if (!Users[username]) {
 		req.session.destroy();
-		res.status(403).json({
+		res.status(404).json({
 			Error: true,
 			Success: false,
-			Message: 'User not found',
+			Message: 'User not found!',
 		});
 	} else if (Users[username] && Users[username] !== password) {
+		req.session.destroy();
 		res.status(403).json({
 			Error: true,
 			Success: false,
-			Message: 'Invalid password and username',
+			Message: 'Invalid username and password!',
 		});
 	} else {
-		res.status(200).json({
+		req.session.User = {
+			Name: username,
+		};
+		res.json({
 			Error: false,
 			Success: true,
 			Message: {
@@ -32,6 +50,40 @@ user.post('/login', (req, res) => {
 			},
 		});
 	}
+});
+user.post('/register', (req, res) => {
+	const { username, password } = req.body;
+	if (!Users[username]) {
+		if (username.length < 4 || password.length < 4) {
+			res.status(400).json({
+				Error: true,
+				Success: false,
+				Message:
+					'Both username and password must be at least 4 characters long.',
+			});
+		} else {
+			Users[username] = password;
+			res.status(201).json({
+				Error: false,
+				Success: true,
+				Message: 'Created user ' + username + '.',
+			});
+		}
+	} else {
+		res.status(409).json({
+			Error: true,
+			Success: false,
+			Message: 'User ' + username + ' already exists.',
+		});
+	}
+});
+user.post('/logout', (req, res) => {
+	req.session.destroy();
+	res.status(202).json({
+		Error: false,
+		Success: true,
+		Message: 'User successfully logged out.',
+	});
 });
 
 module.exports = user;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
 const api = require('./routes/api');
 const app = express();
 const port = 3100;
-const session = require('express-session');
+
 app.use(express.json());
 app.use(morgan('dev'));
-
-app.listen(port, () => {
-	console.log(`Server started at port ${port}`);
-});
-app.use('/api', api);
-
 app.use(
 	session({
 		secret: 'keyboard cat',
@@ -21,6 +16,12 @@ app.use(
 	})
 );
 
+app.use('/api', api);
+
 app.get('/', (req, res) => {
 	res.json('Welcome');
+});
+
+app.listen(port, () => {
+	console.log(`Server started at port ${port}`);
 });
