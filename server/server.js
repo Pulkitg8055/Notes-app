6ca1989572f1483424d7c593030f87d7a3ea2d7b
Pulkit Gupta <pulkitg8055@gmail.com>
2020-12-24 12:01:25 +0530

    Created basic api server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3100;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+	console.log(`Server started at port ${port}`);
+});
+
+app.get('/', (req, res) => {
+	res.json('Hello');
+});
